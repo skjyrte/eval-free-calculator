520ced57f70cac1640ea9875adb3ed950e1d9367
skjyrte <103749024+skjyrte@users.noreply.github.com>
2023-06-03 18:30:11 +0200

    eval version added

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,8 +1,4 @@
 let currentValue: string = "0";
-let firstVariable: number = 0;
-let secondVariable: number = 0;
-let currentOperand: string = "";
-let counter: number = 0;
 
 document.querySelector(".keyboard").addEventListener("click", (event) => {
   if ((event.target as HTMLInputElement).value != undefined) {
@@ -11,36 +7,53 @@ document.querySelector(".keyboard").addEventListener("click", (event) => {
 });
 
 function checker(value: string) {
-  if (value.match(new RegExp(/^[0-9|.]$/g))) {
+  if (value.match(new RegExp(/^[0-9|.|]$/g))) {
     insertValues(value);
   }
+
+  if (value.match(new RegExp(/[+|\-|\/|*]$/g))) {
+    insertOperator(value);
+  }
+
   if (value === "del") {
     deleteCharacter(value);
   }
   if (value === "reset") {
     resetValues();
   }
-  if (value.match(new RegExp(/^[*|/|+|-]$/g))) {
-    setOperand(value);
-  }
-  if (value.match(new RegExp(/^[*|/|+|-]$/g))) {
-    setOperand(value);
-  }
   if (value === "=") {
-    equalsFunction();
+    calculate();
   }
-  console.log(`currentValue: ${currentValue}`);
-  console.log(`currentOperand: ${currentOperand}`);
+  document.querySelector(".resultBox").innerHTML = currentValue;
 }
 
 function insertValues(value: string) {
+  let lastIndex: number = Math.max(
+    currentValue.lastIndexOf("-"),
+    currentValue.lastIndexOf("+"),
+    currentValue.lastIndexOf("*"),
+    currentValue.lastIndexOf("/")
+  );
   if (value === ".") {
     currentValue =
-      currentValue.indexOf(".") === -1 ? currentValue + value : currentValue;
+      currentValue
+        .substring(lastIndex, currentValue.length - 1)
+        .indexOf(".") === -1
+        ? currentValue + value
+        : currentValue;
   } else {
     currentValue = currentValue !== "0" ? currentValue + value : value;
   }
 }
+
+function insertOperator(value: string) {
+  currentValue = currentValue[currentValue.length - 1].match(
+    new RegExp(/[+|\-|\/|*]$/g)
+  )
+    ? currentValue
+    : currentValue + value;
+}
+
 function deleteCharacter(value: string) {
   currentValue = currentValue.slice(0, -1);
   if (currentValue === "") {
@@ -50,71 +63,9 @@ function deleteCharacter(value: string) {
 
 function resetValues() {
   currentValue = "0";
-  firstVariable = 0;
-  secondVariable = 0;
-  currentOperand = "";
-  counter = 0;
-}
-function setOperand(value: string) {
-  if (value === "*") {
-    currentOperand = "*";
-  }
-  if (value === "/") {
-    currentOperand = "/";
-  }
-  if (value === "+") {
-    currentOperand = "+";
-  }
-  if (value === "-") {
-    currentOperand = "-";
-  }
-  if (currentValue !== "" && counter === 0) {
-    secondVariable = firstVariable;
-    firstVariable = parseFloat(currentValue);
-    currentValue = "";
-    counter++;
-  }
-  if (currentValue !== "" && counter > 0) {
-    secondVariable = firstVariable;
-    firstVariable = parseFloat(currentValue);
-    currentValue = "";
-    calculate();
-    counter++;
-  }
-  console.log(
-    `firstVariable: ${firstVariable} secondVariable: ${secondVariable} currentValue: ${currentValue}`
-  );
 }
 
 function calculate() {
-  if (currentOperand === "*") {
-    firstVariable = secondVariable * firstVariable;
-  }
-  if (currentOperand === "/") {
-    firstVariable = secondVariable / firstVariable;
-  }
-  if (currentOperand === "-") {
-    firstVariable = secondVariable - firstVariable;
-  }
-  if (currentOperand === "+") {
-    firstVariable = secondVariable + firstVariable;
-  }
-  console.log(`firstVariable: ${firstVariable}`);
-}
-
-function equalsFunction() {
-  if (currentValue !== "" && counter === 0) {
-    secondVariable = firstVariable;
-    firstVariable = parseFloat(currentValue);
-    currentValue = "";
-    counter++;
-  }
-  if (currentValue !== "" && counter > 0) {
-    secondVariable = firstVariable;
-    firstVariable = parseFloat(currentValue);
-    currentValue = secondVariable.toString();
-    calculate();
-    counter++;
-  }
-  calculate();
+  let resultValue: number = eval(currentValue);
+  currentValue = resultValue.toString();
 }
