3e5e5aef1472fdc2e2b8e8aaa5fd28c59f91e589
skjyrte <103749024+skjyrte@users.noreply.github.com>
2023-06-05 21:25:47 +0200

    1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import "./styles/reset.css";
-import "./styles/styles.scss";
+//import "./styles/reset.css";
+//import "./styles/styles.scss";
 import "./styles/testStyles.css";
 
 import "./theme.ts";
